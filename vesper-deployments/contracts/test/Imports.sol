68fbe361908723251d16b51bcb82b925630cffc5
Virendra Patidar <virendra@bloq.com>
2022-09-19 22:46:58 +0400

    Deploy Compound_Leverage_DAI for vaDAI pool

--- vesper-deployments/contracts/test/Imports.sol
+++ vesper-deployments/contracts/test/Imports.sol
@@ -20,6 +20,7 @@ import "vesper-strategies/contracts/strategies/aave/v2/AaveV2VesperXy.sol";
 import "vesper-strategies/contracts/strategies/convex/2Pool/Convex2PlainPool.sol";
 import "vesper-strategies/contracts/strategies/vesper/earn/VesperEarnSaddleLp.sol";
 import "vesper-strategies/contracts/strategies/aave/v2/AaveV2.sol";
+import "vesper-strategies/contracts/strategies/compound/CompoundLeverage.sol";
 
 // Importer contract to compile the solidity files from dependent `vesper-pools` and `vesper-strategies` modules.
 // solhint-disable-next-line no-empty-blocks
