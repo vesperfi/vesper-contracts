298ba3b3097eee13205bc682c6379d4a097e3b51
Virendra Patidar <virendra@bloq.com>
2022-11-16 18:18:27 +0530

    Deploy Euler Borrow Strategy to vaETH

--- vesper-deployments/contracts/test/Imports.sol
+++ vesper-deployments/contracts/test/Imports.sol
@@ -27,6 +27,7 @@ import "vesper-strategies/contracts/interfaces/maker/IMakerDAO.sol";
 import "vesper-strategies/contracts/strategies/convex/Convex.sol";
 import "vesper-strategies/contracts/strategies/curve/Curve.sol";
 import "vesper-strategies/contracts/strategies/compound/bsc/VenusBNB.sol";
+import "vesper-strategies/contracts/strategies/euler/Euler.sol";
 
 // Importer contract to compile the solidity files from dependent `vesper-pools` and `vesper-strategies` modules.
 // solhint-disable-next-line no-empty-blocks
