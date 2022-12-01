65ba6c8e8908d0050fcf944102b396999b114a31
Rohit Solia <rohit.solia@gmail.com>
2022-12-01 11:09:55 -0600

    Removed renBTC 1.0 tests

--- vesper-strategies/test/avalanche/curve/2pool/ren-pool-wbtce.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const testRunner = require('../../../utils/testRunner')
-
-describe('ren VAWBTCe Pool', function () {
-  testRunner('VAWBTCe', ['Curve_ren_WBTCe'], [{ debtRatio: 10000 }])
-})
