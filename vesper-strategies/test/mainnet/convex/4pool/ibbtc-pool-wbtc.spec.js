9dfbb15e12e281d203b905ba4e944a087c2b3d7a
Marcelo Morgado <cmarcelom@gmail.com>
2023-02-02 19:05:41 +0000

    Remove deprecated Convex pool

--- vesper-strategies/test/mainnet/convex/4pool/ibbtc-pool-wbtc.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const testRunner = require('../../../utils/testRunner')
-
-describe('ibbtc VAWBTC Pool', function () {
-  testRunner('VAWBTC', ['Convex_ibBTC_WBTC'], [{ debtRatio: 10000 }])
-})
