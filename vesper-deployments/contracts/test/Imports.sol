cb056af402d7300eb8a13ff67f7aacef291fc29c
Virendra Patidar <virendra@bloq.com>
2023-02-20 16:05:23 +0400

    Deployed vaETH pool with Curve strategy on Optimism

--- vesper-deployments/contracts/test/Imports.sol
+++ vesper-deployments/contracts/test/Imports.sol
@@ -37,6 +37,7 @@ import "vesper-strategies/contracts/strategies/aave/v3/AaveV3VesperXy.sol";
 import "vesper-strategies/contracts/strategies/convex/ConvexForFrax.sol";
 import "vesper-strategies/contracts/strategies/alpha/AlphaHomora.sol";
 import "vesper-strategies/contracts/strategies/compound/v3/CompoundV3VesperXy.sol";
+import "vesper-strategies/contracts/strategies/curve/CurveETH.sol";
 
 // Importer contract to compile the solidity files from dependent `vesper-pools` and `vesper-strategies` modules.
 // solhint-disable-next-line no-empty-blocks
