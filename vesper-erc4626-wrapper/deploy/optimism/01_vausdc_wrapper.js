276ba6cc2f2693525c9d15ed4c3b352ddf0634d5
Rohit Solia <rohit.solia@gmail.com>
2023-05-31 19:48:18 +0530

    Updated wrapper deploy script

--- vesper-erc4626-wrapper/deploy/optimism/01_vausdc_wrapper.js
+++ vesper-erc4626-wrapper/deploy/optimism/01_vausdc_wrapper.js
@@ -1,57 +1,17 @@
-/* eslint-disable no-console */
 'use strict'
 
-const VPoolWrapper = 'VPoolWrapper'
-const PoolRewardsWrapper = 'PoolRewardsWrapper'
-
-const VPoolWrapperUpgrader = 'VPoolWrapperUpgrader'
-const PoolRewardsWrapperUpgrader = 'PoolRewardsWrapperUpgrader'
-
+const deployWrapper = require('../deploy-wrapper')
 const Address = require('vesper-commons/utils/chains').getChainData().address
 
 const deployFunction = async function (hre) {
-  const { getNamedAccounts, deployments, run } = hre
-
-  const { deploy } = deployments
-  const { deployer } = await getNamedAccounts()
-
-  const VPoolWrapperProxy = await deploy('vaUSDCWrapper', {
-    from: deployer,
-    log: true,
-    proxy: {
-      proxyContract: 'OpenZeppelinTransparentProxy',
-      viaAdminContract: VPoolWrapperUpgrader,
-      implementationName: VPoolWrapper,
-      execute: {
-        init: {
-          methodName: 'initialize',
-          args: [Address.Vesper.vaUSDC],
-        },
-      },
-    },
-  })
-
-  const RewardsWrapperProxy = await deploy('vaUSDCRewardsWrapper', {
-    from: deployer,
-    log: true,
-    // proxy deployment
-    proxy: {
-      proxyContract: 'OpenZeppelinTransparentProxy',
-      viaAdminContract: PoolRewardsWrapperUpgrader,
-      implementationName: PoolRewardsWrapper,
-      execute: {
-        init: {
-          methodName: 'initialize',
-          args: [VPoolWrapperProxy.address, [Address.OP]],
-        },
-      },
-    },
-  })
-
-  console.log('Verifying source code on etherscan')
-  await run('verify', { address: VPoolWrapperProxy.address, noCompile: true })
-  await run('verify', { address: RewardsWrapperProxy.address, noCompile: true })
+  const wrapperConfig = {
+    collateralToken: Address.Vesper.vaUSDC,
+    rewardsToken: Address.OP,
+    vPoolWrapperAlias: 'vaUSDCWrapper',
+    poolRewardsWrapperAlias: 'vaUSDCRewardsWrapper',
+  }
+  await deployWrapper(hre, wrapperConfig)
 }
 
 module.exports = deployFunction
-module.exports.tags = [VPoolWrapper]
+module.exports.tags = ['vaUSDCWrapper']
