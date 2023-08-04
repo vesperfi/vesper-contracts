2c43b2eb8e6a2f8150bb6f0481aa59cc5f8296ec
Rohit Solia <rohit.solia@gmail.com>
2023-08-04 09:39:33 +0530

    Updated BUSD whale address

--- vesper-commons/utils/balance.js
+++ vesper-commons/utils/balance.js
@@ -83,7 +83,7 @@ const whales = {
   [AvalancheAddress.Stargate.STG]: '0x2B065946d41ADf43BBc3BaF8118ae94Ed19D7A40',
 
   // BSC
-  [BscAddress.BUSD]: '0xf977814e90da44bfa03b6295a0616a897441acec',
+  [BscAddress.BUSD]: '0x0000000000000000000000000000000000001004',
   [BscAddress.WBNB]: '0x0ed7e52944161450477ee417de9cd3a859b14fd0',
   [BscAddress.Ellipsis.EPX]: '0xF977814e90dA44bFA03b6295A0616a897441aceC',
   [BscAddress.Alpaca.ALPACA]: '0xb7d85ab25b9d478961face285fa3d8aaecad24a9',
