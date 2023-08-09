920b9d5ab7d27d56260c7e1641594c8ba4cbcff6
Rohit Solia <rohit.solia@gmail.com>
2023-08-09 23:21:44 +0530

    Removed Operator in favor of Keeper contract

--- vesper-utils/deploy/helper.js
+++ vesper-utils/deploy/helper.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-console */
 'use strict'
 
-const Operator = 'Operator'
-const Keeper = 'Keeper'
-
-const deployFunction = async function (hre) {
+const deployContract = async function (hre, contractConfig) {
   const { getNamedAccounts, deployments, run } = hre
+  const { contract, alias, nameArg } = contractConfig
+  const implementationName = `${contract}_Implementation`
 
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
-  const deployed = await deploy(Keeper, {
+  const deployed = await deploy(alias, {
+    contract,
     from: deployer,
     log: true,
     proxy: {
       proxyContract: 'OpenZeppelinTransparentProxy',
-      implementationName: Operator,
+      implementationName,
       execute: {
         init: {
           methodName: 'initialize',
-          args: [Keeper],
+          args: [nameArg],
         },
       },
     },
@@ -28,5 +28,4 @@ const deployFunction = async function (hre) {
   console.log('Verifying source code on etherscan')
   await run('verify', { address: deployed.address, noCompile: true })
 }
-module.exports = deployFunction
-module.exports.tags = [Keeper]
+module.exports = deployContract
