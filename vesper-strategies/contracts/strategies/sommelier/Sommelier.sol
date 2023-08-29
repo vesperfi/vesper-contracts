59ae4a342100c4c563ff49d0f1ded04f43972e93
Manoj <patidarmanoj@gmail.com>
2023-08-28 17:09:29 -0700

    Using sommelier base contract

--- vesper-strategies/contracts/strategies/sommelier/Sommelier.sol
+++ vesper-strategies/contracts/strategies/sommelier/Sommelier.sol
@@ -3,25 +3,23 @@
 pragma solidity 0.8.9;
 
 import "../Strategy.sol";
+import "./SommelierBase.sol";
 import "../../interfaces/sommelier/ISommelier.sol";
 
 /// @dev This strategy will deposit collateral token in Sommelier and earn yield.
-contract Sommelier is Strategy {
+contract Sommelier is Strategy, SommelierBase {
     using SafeERC20 for IERC20;
     // solhint-disable-next-line var-name-mixedcase
     string public NAME;
-    string public constant VERSION = "5.1.0";
-    // Same as receiptToken but it's immutable and saves gas
-    ICellar public immutable cellar;
+    string public constant VERSION = "5.1.1";
 
     constructor(
         address pool_,
         address swapper_,
         address receiptToken_,
         string memory name_
-    ) Strategy(pool_, swapper_, receiptToken_) {
+    ) Strategy(pool_, swapper_, receiptToken_) SommelierBase(receiptToken_) {
         require(ICellar(receiptToken_).asset() == address(IVesperPool(pool_).token()), "invalid-receipt-token");
-        cellar = ICellar(receiptToken_);
         NAME = name_;
     }
 
@@ -30,14 +28,7 @@ contract Sommelier is Strategy {
     }
 
     function tvl() public view virtual override returns (uint256) {
-        return _getCollateralInCellar() + collateralToken.balanceOf(address(this));
-    }
-
-    /**
-     * @notice Time when withdraw and transfer will be unlocked.
-     */
-    function unlockTime() public view returns (uint256) {
-        return cellar.userShareLockStartTime(address(this)) + cellar.shareLockPeriod();
+        return _getAssetsInSommelier() + collateralToken.balanceOf(address(this));
     }
 
     /// @notice Large approval of token
@@ -49,17 +40,13 @@ contract Sommelier is Strategy {
     //solhint-disable no-empty-blocks
     function _beforeMigration(address _newStrategy) internal override {}
 
-    function _getCollateralInCellar() internal view returns (uint256) {
-        return cellar.convertToAssets(cellar.balanceOf(address(this)));
-    }
-
     function _rebalance() internal override returns (uint256 _profit, uint256 _loss, uint256 _payback) {
         uint256 _excessDebt = IVesperPool(pool).excessDebt(address(this));
         uint256 _totalDebt = IVesperPool(pool).totalDebtOf(address(this));
 
         uint256 _collateralHere = collateralToken.balanceOf(address(this));
 
-        uint256 _totalCollateral = _getCollateralInCellar() + _collateralHere;
+        uint256 _totalCollateral = _getAssetsInSommelier() + _collateralHere;
 
         if (_totalCollateral > _totalDebt) {
             _profit = _totalCollateral - _totalDebt;
@@ -86,16 +73,6 @@ contract Sommelier is Strategy {
 
     /// @dev Withdraw collateral here
     function _withdrawHere(uint256 requireAmount_) internal override {
-        if (block.timestamp >= unlockTime()) {
-            // withdraw asking more than available liquidity will fail. To do safe withdraw, check
-            // requireAmount_ against available liquidity.
-            uint256 _withdrawable = Math.min(
-                requireAmount_,
-                Math.min(_getCollateralInCellar(), cellar.totalAssetsWithdrawable())
-            );
-            if (_withdrawable > 0) {
-                cellar.withdraw(_withdrawable, address(this), address(this));
-            }
-        }
+        _withdrawFromSommelier(requireAmount_);
     }
 }
