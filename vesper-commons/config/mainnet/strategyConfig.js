45e123678929152a470c7dc4762f624e60333cae
Manoj <patidarmanoj@gmail.com>
2023-08-29 20:57:11 -0700

    Sommelier strategy of varETH

--- vesper-commons/config/mainnet/strategyConfig.js
+++ vesper-commons/config/mainnet/strategyConfig.js
@@ -241,6 +241,21 @@ const StrategyConfig = {
     setup: { ...setup },
   },
 
+  AaveV3_Sommelier_Xy_RETH_WETH: {
+    contract: 'AaveV3SommelierXy',
+    type: StrategyTypes.AAVE_V3_SOMMELIER_XY,
+    constructorArgs: {
+      swapper,
+      receiptToken: Address.Aave.V3.aEthrETH,
+      borrowToken: Address.WETH,
+      aaveAddressProvider: Address.Aave.V3.AddressProvider,
+      cellar: Address.Sommelier.YieldETH,
+      strategyName: 'AaveV3_Sommelier_Xy_RETH_WETH',
+    },
+    config: { ...config }, // Shallow copy
+    setup: { ...setup },
+  },
+
   AaveV3_Vesper_Xy_RETH_USDC: {
     contract: 'AaveV3VesperXy',
     type: StrategyTypes.AAVE_V3_VESPER_XY,
