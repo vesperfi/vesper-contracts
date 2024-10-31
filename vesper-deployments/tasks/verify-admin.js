4e22653a7457ed2e894acd341d4d960e0169ec3d
Rohit Solia <rohit.solia@gmail.com>
2024-10-30 22:42:58 -0500

    Updated verify-admin script and its output format

--- vesper-deployments/tasks/verify-admin.js
+++ vesper-deployments/tasks/verify-admin.js
@@ -4,10 +4,22 @@ const fs = require('fs')
 const ADMIN_SLOT = '0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103'
 const IMPLEMENTATION_SLOT = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
 const safe = {
-  avalanche: '0x98ca142b7a7856375d665B58A64FB6D29b49eF1f',
   mainnet: '0x9520b477Aa81180E6DdC006Fc09Fb6d3eb4e807A',
   optimism: '0x32934AD7b1121DeFC631080b58599A0eaAB89878',
+  base: '0x32934AD7b1121DeFC631080b58599A0eaAB89878',
 }
+
+// ethers getNetwork().name returns unknown for 'base'
+async function getChain() {
+  const chain = {
+    1: 'mainnet',
+    10: 'optimism',
+    8453: 'base',
+  }
+  const chainId = (await ethers.provider.getNetwork()).chainId
+  return chain[chainId]
+}
+
 async function getProxyAdminAddress(proxyAddress) {
   const proxyAdminStorage = (await ethers.provider.getStorageAt(proxyAddress, ADMIN_SLOT)).toString()
   if (proxyAdminStorage.length === 42) {
@@ -31,25 +43,32 @@ async function getOwner(address) {
 
 async function runValidation(input, safeAddress) {
   if (!input) {
-    return {}
+    return null
   }
   const result = {}
-  result.proxy = input.proxy
   const liveAdmin = await getProxyAdminAddress(input.proxy)
-  result.proxyAdmin = liveAdmin
   if (liveAdmin !== input.proxyAdmin) {
+    result.newProxyAdmin = liveAdmin
     result.incorrectAdminInRelease = true
+    result.action = 'updateReleaseFile'
   }
+
   const owner = await getOwner(liveAdmin)
-  result.proxyAdminOwner = owner
-  result.isSafeProxyAdminOwner = owner === safeAddress
+  if (owner !== safeAddress) {
+    result.proxyAdminOwner = owner
+    result.expectedProxyAdminOwner = safeAddress
+    result.changeInProxyAdminRequired = true
+    result.action = 'changeProxyAdmin, update deployer dir name at deployments/{chain}/global'
+  }
 
   const liveImpl = await getImplAddress(input.proxy)
-  result.implementation = liveImpl
   if (liveImpl !== input.implementation) {
+    result.newImplementation = liveImpl
     result.incorrectImplInRelease = true
+    result.action = 'updateReleaseFile'
   }
-  return result
+
+  return Object.keys(result).length > 0 ? result : null
 }
 
 task('verify-admin', 'Verify proxy admins are okay')
@@ -58,8 +77,8 @@ task('verify-admin', 'Verify proxy admins are okay')
     if (!process.env.NODE_URL) {
       throw Error('Please set NODE_URL in env')
     }
-    const chainFromProvider = (await ethers.provider.getNetwork()).name
-    const chain = chainFromProvider === 'homestead' ? 'mainnet' : chainFromProvider
+
+    const chain = await getChain()
 
     const releaseFile = `./releases/${release}/contracts.json`
 
@@ -72,33 +91,50 @@ task('verify-admin', 'Verify proxy admins are okay')
 
     const data = releaseJson.networks[chain]
     const safeAddress = safe[chain]
-    const keys = Object.keys(data)
+    const poolNames = Object.keys(data)
     const result = {}
 
-    for (let key of keys) {
-      result[key] = {}
+    for (let poolName of poolNames) {
+      result[poolName] = {}
 
-      const poolData = data[key]
-      const pool = poolData.pool
+      const poolData = data[poolName]
 
-      result[key].pool = await runValidation(pool, safeAddress)
-      result[key].pool.poolAccountant = await runValidation(pool.poolAccountant, safeAddress)
-      const poolRewards = await runValidation(poolData.poolRewards, safeAddress)
-      if (Object.keys(poolRewards).length > 0) {
-        result[key].poolRewards = poolRewards
+      const resultPool = await runValidation(poolData.pool, safeAddress)
+      if (resultPool) {
+        result[poolName].pool = resultPool
+      }
+
+      const resultPoolAccountant = await runValidation(poolData.pool.poolAccountant, safeAddress)
+      if (resultPoolAccountant) {
+        if (!result[poolName].pool) {
+          result[poolName].pool = {}
+        }
+        result[poolName].pool.poolAccountant = resultPoolAccountant
+      }
+
+      const resultPoolRewards = await runValidation(poolData.poolRewards, safeAddress)
+      if (resultPoolRewards) {
+        result[poolName].poolRewards = resultPoolRewards
+      }
+
+      // if result is empty for pool, then delete the empty object
+      if (Object.keys(result[poolName]).length === 0) {
+        delete result[poolName]
       }
     }
     let output = {}
-    const dir = `./verify-admin/${release}`
-    const outputFileName = `${dir}/output.json`
+    const dir = './verify-admin-output'
+    const outputFileName = `${dir}/${release}-action-item.json`
 
     if (fs.existsSync(outputFileName)) {
       output = JSON.parse(fs.readFileSync(outputFileName))
+      output.date = new Date()
       output.networks[chain] = result
     } else {
       fs.mkdirSync(dir, { recursive: true })
       output = {
         version: release,
+        date: new Date(),
         networks: {
           [chain]: result,
         },
