4c4c22a2a70b55dbcd21122c621116525b2d0d06
Rohit Solia <rohit.solia@gmail.com>
2024-11-15 01:34:37 +0530

    Added Fraxlend V3 strategy

--- vesper-strategies/test/mainnet/frax-lend/vafrax.spec.js
+++ vesper-strategies/test/mainnet/frax-lend/vafrax.spec.js
@@ -4,4 +4,5 @@ const testRunner = require('../../utils/testRunner')
 
 describe('vaFrax Pool with FraxLend strategy', function () {
   testRunner('VAFRAX', ['FraxLend_CRV_FRAX'], [{ debtRatio: 10000 }])
+  testRunner('VAFRAX', ['Fraxlend_sfrxETH_FRAX'], [{ debtRatio: 10000 }])
 })
